9e29759261555cd497d168af7124b8515c87c60f
Nathalie Ayala Santana <nathalieaysa@gmail.com>
2013-09-03 22:59:56 -0500

    creando usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Toheroku::Application.routes.draw do
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
